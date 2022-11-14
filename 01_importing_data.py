a033be8065a4b83a0faa2eddf43ac10917fd6aa3
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2022-11-13 20:30:01 -0500

    Importing csv files

---
+++ 01_importing_data.py
@@ -0,0 +1,10 @@
+## Learning Python with National Parks Biodiversity
+
+# Load libraries
+import pandas as pd
+
+# Read in parks
+parks = pd.read_csv('data/parks.csv')
+
+# Read in species
+species = pd.read_csv('data/species.csv')
